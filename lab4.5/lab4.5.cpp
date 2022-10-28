a8155cc8918bfe5391e28a7ee1f47c2f5dd5aafa
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-10-28 12:38:11 +0300

    lab4.5

---
+++ lab4.5/lab4.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i <= 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if ((y >= 1 && y <= 3 && x >= -1 && x <= 1) ||
+			(x * x + y * y <= 1) ||
+			(y >= -2 && y <= x - 1 && y <= -x - 1))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	cout << "R = "; cin >> R;
+		for (int i = 0; i < 10; i++)
+		{
+			x = 2 * R * rand() / RAND_MAX - R;
+			y = 2 * R * rand() / RAND_MAX - R;
+			if ((y <= sqrt(R * R - x * x) && y >= x && x >= 0) ||
+				(y >= -sqrt(R * R - x * x) && y <= x && x <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
